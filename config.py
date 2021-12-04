763d600b0108627d7aab96c3aa03a2d03686f959
nebelorz <nebelorz@hotmail.com>
2021-12-04 15:21:44 +0100

    UPDATED

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import random
 ####################
 class settings():
     CAST_STRENGTH = random.uniform(1.7, 1.9)    # (1.9, 1.9) to always land MAX)
-    CASTS_TO_REPAIR = 15                        # Casts until the fishing rod is repaired
+    CASTS_TO_REPAIR = 60                        # Casts until the fishing rod is repaired
 
     SET_BAIT = None     # None -- Will skip attaching a bait
                         # True -- Will attach a bait everytime it expires
