c047d3c42ad6aa6438c899d33d3e1f7227d2816c
nebelorz <nebelorz@hotmail.com>
2021-12-31 14:48:56 +0100

    removed delay

--- console.py
+++ console.py
@@ -13,5 +13,4 @@ win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0, 0, 500, 250, win32con.SWP_
 
 
 subprocess.call('cls', shell=True)
-time.sleep(0.5)
 subprocess.call('python main.py', shell=True)
