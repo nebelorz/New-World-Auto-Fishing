e1061d65025d7a7528aa8adee3fe5feafd9eff3c
nebelorz <nebelorz@hotmail.com>
2021-12-31 14:45:46 +0100

    removed pyGetWindow library

--- get_window.py
+++ get_window.py
@@ -1,6 +1,4 @@
-import time
 import pyautogui as pyAG
-import pygetwindow as pyGW
 
 screen_width, screen_height = pyAG.size() # Screen dimmensions
 
@@ -9,24 +7,17 @@ def cursor_to_screen():
 
 def check_window(self):
     try:
-        pyGW.getWindowsWithTitle(self)[0]
+        pyAG.getWindowsWithTitle(self)[0]
         return(True)
     except:
         print(self, 'not found')
         return(False)
-        
+
 def get_window(self):
-    window = pyGW.getWindowsWithTitle(self)[0]
+    window = pyAG.getWindowsWithTitle(self)[0]
+
     if window.isMinimized:
-        window.restore()
         window.activate()
         window.maximize()
-        time.sleep(0.5)
     else:
         window.activate()
-        window.maximize()
-        time.sleep(0.5)
-
-def minimize_window(self):
-    window = pyGW.getWindowsWithTitle(self)[0]
-    window.minimize()
